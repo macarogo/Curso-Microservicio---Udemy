dd03fc6ce976142cbd9900e9fb027fd3df0d1bde
macarogo <mariacarolinagomez88@gmail.com>
2022-12-12 20:59:55 -0300

    Modify Request

--- springboot-service-gatewayy-server/src/main/java/com/trainingbdi/springboot/app/gateway/filters/ExampleGlobalFilter.java
+++ springboot-service-gatewayy-server/src/main/java/com/trainingbdi/springboot/app/gateway/filters/ExampleGlobalFilter.java
@@ -1,9 +1,12 @@
 package com.trainingbdi.springboot.app.gateway.filters;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,18 +15,29 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class ExampleGlobalFilter implements GlobalFilter{
+public class ExampleGlobalFilter implements GlobalFilter, Ordered{
 	
 	private final  Logger logger= LoggerFactory.getLogger(ExampleGlobalFilter.class);
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		logger.info("running pre filter");
+		exchange.getRequest().mutate().headers(h -> h.add("token","654321"));
 		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
 			logger.info("running post fiter");
+			
+			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
+				exchange.getResponse().getHeaders().add("token", valor);
+			});
+			
 			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "red").build());
 			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
+	}
+
+	@Override
+	public int getOrder() {
+		return -1;
 	}
 
 }

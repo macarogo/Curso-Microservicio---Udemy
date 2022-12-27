370ae1e641ad41f4e18a2314994a94b6276bf695
macarogo <mariacarolinagomez88@gmail.com>
2022-12-27 17:11:32 -0300

    CircuitBreaker and Timel Limiter annotation

--- springboot-service-item/src/main/java/com/trainingbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-service-item/src/main/java/com/trainingbdi/springboot/app/item/controllers/ItemController.java
@@ -1,6 +1,7 @@
 package com.trainingbdi.springboot.app.item.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.trainingbdi.springboot.app.item.models.Item;
 import com.trainingbdi.springboot.app.item.models.Products;
 import com.trainingbdi.springboot.app.item.models.service.ItemService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 public class ItemController {
@@ -36,11 +40,23 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
-	//@HystrixCommand(fallbackMethod="metodoAlternativo")
 	@GetMapping("/products/{id}/amount/{amount}")
 	public Item detail(@PathVariable Long id, @PathVariable Integer amount) {
 		return cbFactory.create("items")
 				.run(()-> itemService.findBy(id, amount), e -> metodoAlternativo(id, amount, e));
+	}
+	
+	@CircuitBreaker(name= "items", fallbackMethod = "metodoAlternativo")
+	@GetMapping("/products2/{id}/amount/{amount}")
+	public Item detail2(@PathVariable Long id, @PathVariable Integer amount) {
+		return itemService.findBy(id, amount);
+	}
+	
+	@CircuitBreaker(name= "items", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name= "items", fallbackMethod = "metodoAlternativo2")
+	@GetMapping("/products3/{id}/amount/{amount}")
+	public CompletableFuture<Item> detail3(@PathVariable Long id, @PathVariable Integer amount) {
+		return CompletableFuture.supplyAsync(() -> itemService.findBy(id, amount));
 	}
 	
 	public Item metodoAlternativo(@PathVariable Long id, @PathVariable Integer amount, Throwable e) {
@@ -54,6 +70,19 @@ public class ItemController {
 		products.setPrice(500.00);
 		item.setProducts(products);
 		return item;
+	}
+	
+	public CompletableFuture<Item> metodoAlternativo2(@PathVariable Long id, @PathVariable Integer amount, Throwable e) {
+		logger.info(e.getMessage());
+		Item item= new Item();
+		Products products= new Products();
+		
+		item.setAmount(amount);
+		products.setId(id);
+		products.setName("Camara sony");
+		products.setPrice(500.00);
+		item.setProducts(products);
+		return CompletableFuture.supplyAsync(() -> item);
 	}
 
 }

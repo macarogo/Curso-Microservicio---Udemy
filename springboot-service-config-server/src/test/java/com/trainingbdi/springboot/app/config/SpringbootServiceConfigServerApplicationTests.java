71ce653a74042ed0784ad0892ba62ad0a3de4159
mariacarolinagomez88 <mariacarolinagomez88@gmail.com>
2023-01-03 20:36:33 -0300

    Spring Cloud config server

---
+++ springboot-service-config-server/src/test/java/com/trainingbdi/springboot/app/config/SpringbootServiceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingbdi.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- springboot-service-item/src/main/java/com/trainingbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-service-item/src/main/java/com/trainingbdi/springboot/app/item/controllers/ItemController.java
@@ -1,13 +1,18 @@
 package com.trainingbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,6 +37,9 @@ public class ItemController {
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	
+	@Value("${configurations.text}")
+	private String text;
 	
 	@GetMapping("/list")
 	public List<Item> lisst(@RequestParam(name="name", required= false)String name, @RequestHeader(name="token-request", required= false)String token){
@@ -83,6 +91,16 @@ public class ItemController {
 		products.setPrice(500.00);
 		item.setProducts(products);
 		return CompletableFuture.supplyAsync(() -> item);
+	}
+	
+	@GetMapping("/get-config")
+	public ResponseEntity<?> getConfig(@Value("${server.port}") String port){
+		logger.info(text);
+		
+		Map<String, String> json= new HashMap<>();
+		json.put("text", text);
+		json.put("port", port);
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 }
